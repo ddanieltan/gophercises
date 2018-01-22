9688379902b8e92eee8d985d0dfa8ca6cf36dcf2
Daniel Tan <dtan@tripadvisor.com>
2018-01-22 17:58:12 +0800

    got part 1 working

--- Ex01-Quiz-Game/main.go
+++ Ex01-Quiz-Game/main.go
@@ -3,36 +3,52 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
+	// "io"
 	"os"
 )
+
+type problem struct {
+	ques string
+	ans  string
+}
 
 func main() {
 
 	file, err := os.Open("problems.csv")
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		fmt.Println("Failed to open csv")
+		os.Exit(1)
 	}
 	defer file.Close()
 
 	r := csv.NewReader(file)
-
-	for {
-		record, err := r.Read()
-
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-		ques := record[0]
-		ans := record[1]
-		fmt.Printf("\nQuestion: %v Answer: %v", ques, ans)
+	lines, err := r.ReadAll()
+	if err != nil {
+		fmt.Println("Failed to open csv reader")
+		os.Exit(1)
 	}
+
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			ques: line[0],
+			ans:  line[1],
+		}
+	}
+
+	var score int //default value is 0
+	for i, prob := range problems {
+		fmt.Printf("Question #%s\n%s\n", i, prob.ques)
+		var user_ans string
+		fmt.Scanf("%s\n", &user_ans)
+		if user_ans == prob.ans {
+			fmt.Println("Correct!")
+			score++
+		} else {
+			fmt.Println("Incorrect!")
+		}
+
+	}
+	fmt.Printf("You got %d out of %d questions correct", score, len(problems))
 
 }
